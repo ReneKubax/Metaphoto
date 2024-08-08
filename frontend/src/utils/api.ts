fe249d55204d78b4e43eb3787eb7df8fb7950549
Rene Fuentes <145807039+efuentes02@users.noreply.github.com>
2024-08-08 09:56:46 -0600

    Feat(FrontEnd):✨ config env to prod

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/externalapi',
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000/externalapi',
 });
 
 /**
